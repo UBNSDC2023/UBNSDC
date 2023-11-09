a3fc2aa4a6c5b336294c1450fec06e7c209c8a07
Kritarth Dandapat <141005022+Kritarth-Dandapat@users.noreply.github.com>
2023-11-09 13:25:45 -0500

    updated footer and about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import "./style.css"
 
 function About() {
   return (
-    <div>
+    <>
+    <div className='container'>
+
+    <h1 className='header'>About NSDC at UB</h1>
+    
+    <h2>Our Mission</h2>
+    <p>
+    At NSDC, we believe in empowering individuals and communities to drive positive change towards a more sustainable world. Our mission is to:
+    </p>
+    <ul className='list'>
+    <li>Promote awareness and understanding of sustainable development goals.</li>
+    <li>Engage in research and initiatives that address pressing environmental and social challenges.</li>
+    <li>Foster a community of passionate individuals committed to creating a better future for all.</li>
+    </ul>
+    <h2>Our Origins</h2>
+    <p>
+    Founded at UCLA, NSDC has now found its home at the University at Buffalo, where we continue to build upon a legacy of environmental leadership and innovation. With a diverse and dedicated team, we are committed to advancing sustainable practices both locally and globally.
+    </p>
         
     </div>
+    </>
   )
 }
 
