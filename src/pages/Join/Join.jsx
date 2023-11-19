1394657e473467b2b6d9b6315474b7785e4ae9b8
octomaanav <maanav.vashist@gmail.com>
2023-11-19 03:15:21 -0500

    join us page

--- src/pages/Join/Join.jsx
+++ src/pages/Join/Join.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import './Join.css'
 import Navbar from '../../components/Navbar/Navbar'
+import { BsDiscord, BsInstagram, BsTwitter } from "react-icons/bs";
+import { Link } from 'react-router-dom';
+import insta from '../../../assets/instagram.png'
+import groupme from '../../../assets/Group-logo.png'
+import Button from '../../components/Button/Button';
+import { IconContext } from 'react-icons';
 
 function Join() {
   return (
     <div>
         <Navbar/>
         
-    </div>
+      </div>
+    </>
   )
 }
 
