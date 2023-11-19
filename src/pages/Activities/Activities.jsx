f830d444cbee3bd972d4c9a60ace8982264d2122
Kritarth Dandapat <141005022+Kritarth-Dandapat@users.noreply.github.com>
2023-11-18 23:40:11 -0500

    Did couple of changes

---
+++ src/pages/Activities/Activities.jsx
@@ -0,0 +1,21 @@
+import './Activities.module.css'
+import ActivityComponent from './ActivityComponent'
+
+function Activities() {
+        return (
+                <>
+                <div>
+
+                <div className='heading'> Activities and Projects</div>
+
+                    <h2>Upcomign Activites and Projects</h2>  
+
+                    <ActivityComponent  name = "Hello" date="24th Janurary 2023" description="Hello" />
+
+                </div>
+
+                </>
+        )
+}
+
+export default Activities
