289ff0d2f6d4ef75d5f341003178a537d719a74d
Kritarth Dandapat <141005022+Kritarth-Dandapat@users.noreply.github.com>
2023-11-25 15:15:54 -0500

    updates

--- src/pages/Home/FAQComponent.jsx
+++ src/pages/Home/FAQComponent.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react"
+
 function FAQComponent({question, answer}) {
+        const [isDepsOptimizerEnabled, SetIsOpen] = useState(false)
         return (
                 <div>
-                        
+                       <p className="question">{question}</p> 
+
+                       {}
+                       <question className="question">{answer}</question>
                 </div>
         )
 }
